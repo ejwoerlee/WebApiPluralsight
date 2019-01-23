dc1ff003d485650d6986fc91691b94ea3e113db9
E.J. Woerlee <egbert-jan.woerlee@cgm.com>
2019-01-23 14:34:48 +0100

    Exercises / Test

---
+++ Excersises/Module2/NewWebApi/NewWebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NewWebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CompuGroup Medical SE")]
+[assembly: AssemblyProduct("NewWebApi.Tests")]
+[assembly: AssemblyCopyright("Copyright © CompuGroup Medical SE 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eba1451d-2378-467e-9866-32d727088486")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Excersises/Module2/NewWebApi/NewWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Web.Http;
+using System.Web.WebPages;
+using Microsoft.Ajax.Utilities;
+
+namespace NewWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // GET api/values/5
+       
+        public IHttpActionResult Get(int id)
+        {
+            var test = new List<string>() {"een", "twee", "drie"};
+            return Ok(new {name = "value", id = test});
+        }
+
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
